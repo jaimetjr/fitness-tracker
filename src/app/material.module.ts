81689274f9126a8f48ea91c1e5fef29ea2946fe2
Jaime Tasca Junior <jaime.tasca.jr@gmail.com>
2021-03-25 17:59:41 -0300

    creation of pages and material references

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [ MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule ],
+  exports: [ MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule ]
+})
+export class MaterialModule {
+
+}
